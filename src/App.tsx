200e871bfb244456983f3ba1b4c17eb3644a09c0
Atefeh Mohammadi <atefeh.mohammadi28@gmail.com>
2025-12-27 12:40:55 +0330

    fix: remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { useState, useEffect } from "react";
 
